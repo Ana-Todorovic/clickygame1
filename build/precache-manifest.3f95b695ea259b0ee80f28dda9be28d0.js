d87c767e83acc2aa0cd8e05324f4f0dcac5cbfb8
Ana Todorovic <todorovica2137@gmail.com>
2019-11-19 22:39:08 -0800

    updating gitignore

---
+++ build/precache-manifest.3f95b695ea259b0ee80f28dda9be28d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a254dc0ba7d546b799d99544fedbe6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ba2c23d3e024f23568",
+    "url": "/static/css/main.5443a68c.chunk.css"
+  },
+  {
+    "revision": "ad9494b941185911dd41",
+    "url": "/static/js/2.adda3e1c.chunk.js"
+  },
+  {
+    "revision": "74ba2c23d3e024f23568",
+    "url": "/static/js/main.41b6ffe1.chunk.js"
+  },
+  {
+    "revision": "7bbc34b7c756987411d6",
+    "url": "/static/js/runtime-main.8c666951.js"
+  }
+]);
